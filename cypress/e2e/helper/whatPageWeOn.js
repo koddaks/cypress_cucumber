13332829f35cf168be48c36cc491be4071cd5d59
koddaks <koddaks@users.noreply.github.com>
2022-10-05 06:39:08 +0000

    deploy: a073a560790d8af3629dd3b0c5444aa6296cb787

--- cypress/e2e/helper/whatPageWeOn.js
+++
@@ -1,8 +0,0 @@
-class WhatPageWeOn {
-  pageShouldBe(expectedPage){
-    cy.location().should((loc) => {
-      expect(loc.href).to.eq(expectedPage);
-    })
-  }
-}
-module.exports = new WhatPageWeOn();
