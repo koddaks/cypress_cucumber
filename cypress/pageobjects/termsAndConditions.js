b5602c230e2ad16dc0836e38a28ebec712a0e44b
Borysov Kostyantyn <koss199710@gmail.com>
2022-10-06 14:11:46 +0300

    Refactor

--- cypress/pageobjects/termsAndConditions.js
+++ cypress/pageobjects/termsAndConditions.js
@@ -3,6 +3,93 @@ class TermsAndConditions{
       return cy.get('[class="sc-69277c81-0 Va-dul"]');
     }
 
+    get terms(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(2)');
+    }
+    get termsText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(3)');
+    }
+
+
+
+    get useLicense(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(4)');
+    }
+    get useLicenseText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(5)');
+    }
+    
+
+    get Disclaimer(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(6)');
+    }
+    get DisclaimerText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(7)');
+    }
+
+
+    get limitations(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(8)');
+    }
+    get limitationsText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(9)');
+    }
+
+    get revisionsAndErrors(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(10)');
+    }
+    get revisionsAndErrorsText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(11)');
+    }
+
+
+    get links(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(12)');
+    }
+    get linksText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(13)');
+    }
+    get linksRecaptchaText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(14)');
+    }
+
+
+    get modificationsInSiteTermsOfUse(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(15)');
+    }
+    get modificationsInSiteTermsOfUseText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(16)');
+    }
+
+    
+
+    get governingLaw(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(17)');
+    }
+    get governingLawText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(18)');
+    }
+
+
+
+    get indemnification(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(19)');
+    }
+    get indemnificationText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(20)');
+    }
+
+
+
+    get termination(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(21)');
+    }
+    get terminationText(){
+      return cy.get('.sc-d8f5e61f-5 > div > :nth-child(22)');
+    }
+   
+
+
   
 }
 module.exports = new TermsAndConditions();
--- cypress/support/step_definitions/testcases.cy.js
+++ cypress/support/step_definitions/testcases.cy.js
@@ -9,7 +9,7 @@ const whatPageWeOn = require("../../e2e/helper/whatPageWeOn");
 
 Given('I am on the Telnyx page and click the cookies', () => {
   base.navigate();
-  cy.contains('Accept and close').click();
+  // cy.contains('Accept and close').click();
 });
 //Scenario#1
 When('I scroll to “About Us” link at the bottom of the page', async() => {
@@ -62,7 +62,7 @@ Then('I redirected to “About Us” page', async() => {
     cy.contains('Momentum leader and highest satisfaction in G2 CPaaS grid').scrollIntoView();
   });
   Then('I check for a “list of awards and nominations” for summer 2021.', async() => {
-    mainpage.listOfAwardsEaseOfUse.scrollIntoView().should('be.visible');
+    mainpage.listOfAwardsEaseOfUse.scrollIntoView({ duration: 2000 }).should('be.visible');
     mainpage.listOfAwardsBestResults.should('be.visible');
     mainpage.listOfAwardsBestUsability.should('be.visible');
     mainpage.listOfAwardsLeader.should('be.visible');
@@ -77,6 +77,7 @@ Then('I redirected to “About Us” page', async() => {
          });
   Then('I click “Data & Privacy” link.', async() => {
     mainpage.dataPrivacyLink.click();
+    whatPageWeOn.currentUrlShouldInclude('/data-privacy')
          
            });
   Then('I check To the right of the heading "Data & Privacy" image', async() => {
@@ -88,6 +89,7 @@ Then('I redirected to “About Us” page', async() => {
          });
   Then('I click “Report Abuse” link', async() => {
     mainpage.reportAbuseLink.click();
+    whatPageWeOn.currentUrlShouldInclude('/report-abuse')
          
            });
   Then('I check “reCAPTCHA validation”', async() => {    
@@ -106,7 +108,7 @@ Then('I redirected to “About Us” page', async() => {
  
   Then('I click on the first search “result”', async() => {
     supportCenter.searchResultHowSignUp.click();
-    whatPageWeOn.pageShouldBe('https://support.telnyx.com/en/articles/5295540-how-to-sign-up-for-a-telnyx-account');
+    whatPageWeOn.currentUrlShouldInclude('how-to-sign-up-for-a-telnyx-account');
            });
   Then('I scroll to header “Signing up for an account” and find example registration form image under it', async() => {
     supportCenter.searchResultSignUpImage.scrollIntoView({ duration: 2000 }).should('be.visible').screenshot();
@@ -118,9 +120,48 @@ Then('I redirected to “About Us” page', async() => {
   
    Then('I click “Website Terms and Conditions” link.', async() => {
     mainpage.termsAndConditionsLink.click();
+    whatPageWeOn.currentUrlShouldInclude('/terms-and-conditions')
             });
    Then('I scroll to Headline  “Terms and Conditions”', async() => {
-     termsAndConditions.termsAndConditionsHeader.scrollIntoView().screenshot();
+     termsAndConditions.termsAndConditionsHeader.scrollIntoView().screenshot();  
+     termsAndConditions.terms.should('contain', 'Terms');
+     termsAndConditions.termsText.should('contain', 'By accessing this website, you are agreeing to be bound by these website Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trade mark law.');
+
+
+     termsAndConditions.useLicense.should('contain', 'Use License');
+     termsAndConditions.useLicenseText.should('contain', 'Permission is granted to temporarily download one copy of the materials on Telnyx\’s website for personal, non-commercial transitory viewing only. This the grant of a license, not a transfer of title, and under this license you may not: modify or copy the materials, use the materials for any commercial purpose, attempt to decompile or reverse engineer any software contained on Telnyx\’s website, remove any copyright or other proprietary notations from the materials, and transfer the materials to another person or “mirror” the materials on any other server.');
+
+
+     termsAndConditions.Disclaimer.should('contain', 'Disclaimer');
+     termsAndConditions.DisclaimerText.should('contain', 'The materials on Telnyx\’s website are provided “as is”. Telnyx makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Telnyx does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.');
+
+
+     termsAndConditions.limitations.should('contain', 'Limitations');
+     termsAndConditions.limitationsText.should('contain', 'In no event shall Telnyx or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Telnyx\’s website, even if Telnyx or a Telnyx authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.');
+
+
+     termsAndConditions.revisionsAndErrors.should('contain', 'Revisions And Errors');
+     termsAndConditions.revisionsAndErrorsText.should('contain', 'The materials appearing on our website could include technical, typographical, or photographic errors. Telnyx does not warrant that any of the materials on its website are accurate, complete, or current. Telnyx may make changes to the materials contained on its website at any time without notice. Telnyx does not, however, make any commitment to update the materials. We nor any party involved in creating, producing or delivering this website shall be liable for any direct, incidental, consequential, indirect or punitive damages or any damages whatsoever arising out of your access, use or inability to use this site or on any other hyper-linked website, or any errors or omissions in the content thereof.');
+
+
+     termsAndConditions.links.should('contain', 'Links');
+     termsAndConditions.linksText.should('contain', 'Telnyx has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. We are not responsible for the accuracy or reliability of the information on such linked websites and we cannot vouch for the data, opinions, advice or statements made there. Even if we visit those websites, we cannot be responsible for updating or correcting false or misleading information on any third-party web site. Further, we are not responsible for the goods or services that may be offered on other web sites.');
+
+
+     termsAndConditions.modificationsInSiteTermsOfUse.should('contain', 'Modifications In Site Terms Of Use');
+     termsAndConditions.modificationsInSiteTermsOfUseText.should('contain', 'Telnyx may revise these Terms of use for its website at any time without notice. By using our website you are agreeing to be bound by the current version of the Terms and Conditions of Use.');
+
+
+     termsAndConditions.governingLaw.should('contain', 'Governing Law');
+     termsAndConditions.governingLawText.should('contain', 'Any claim relating to Telnyx\’s website shall be governed by the laws of the State of Illinois without regard to its conflict of law provisions.');
+
+
+     termsAndConditions.indemnification.should('contain', 'Indemnification');
+     termsAndConditions.indemnificationText.should('contain', 'You agree to indemnify, defend and hold harmless Telnyx, its parent companies, subsidiaries, affiliated companies, joint venturers, business partners, licensors, employees, and agents from and against all claims, losses, expenses, damages and costs (including, but not limited to, direct, incidental, consequential, exemplary and indirect damages), and reasonable attorneys\’ fees, resulting from or arising out of your use, misuse, or inability to use the website or any violation by you of these Terms.');
+
+
+     termsAndConditions.termination.should('contain', 'Termination');
+     termsAndConditions.terminationText.should('contain', 'Telnyx reserves the right, without notice and in its sole discretion, to terminate your license to use the website and to block or prevent your future access to, and use of, the website.');
             });
   //Scenario#8
   When('I scroll to “Follow on Facebook” link at the bottom of the page', async() => {
@@ -153,5 +194,6 @@ Then('I redirected to “About Us” page', async() => {
   });
   Then('I redirected to “LinkedIn” page', async() => {    
   whatPageWeOn.currentUrlShouldInclude('linkedin.com');
+  cy.screenshot();
   // cy.get('[class="focus-page"]').screenshot(); I have exception. idkn why? but screenshot is done.
 });
